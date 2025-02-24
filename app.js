b054bad7a837a4e605f6037dc85053d13918bb0d
Aissatou Fall <aissatou.fall240@gmail.com>
2025-02-24 12:50:42 +0000

    Ajout du pipeline CI/CD

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello CI/CD!');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello CI/CD!');
+// });
 
-module.exports = app;
+// module.exports = app;
+
+function sum(a, b) {
+    return a + b;
+  }
+  module.exports = sum;
--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,12 @@
-const request = require('supertest');
-const app = require('../app');
+// const request = require('supertest');
+// const app = require('../app');
 
-test('GET / should return Hello CI/CD!', async () => {
-    const response = await request(app).get('/');
-    expect(response.text).toBe('Hello CI/CD!');
+// test('GET / should return Hello CI/CD!', async () => {
+//     const response = await request(app).get('/');
+//     expect(response.text).toBe('Hello CI/CD!');
+// });
+const sum = require('../app');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
 });
